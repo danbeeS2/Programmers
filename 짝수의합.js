5874a55aaa0e9066ed710027d941e9af43a0033c
Danbi <silverwill39@gmail.com>
2022-12-25 20:51:59 -0300

    Lv.0 짝수의 합

---
+++ 짝수의합.js
@@ -0,0 +1,16 @@
+function solution(n) {
+  let answer = 0;
+
+  // n까지의 짝수 배열 만들기
+  const evenSum = (arr) => {
+    for (let i = 1; i < n + 1; i++) {
+      if (i % 2 === 0) arr.push(i);
+    }
+  };
+  let myArr = [];
+  evenSum(myArr);
+
+  // 배열 더하기
+  answer = myArr.reduce((pre, cur) => (pre += cur), 0);
+  return answer;
+}
