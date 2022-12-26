9a474132d4e858886a23dcd1edecb4c0470895b1
Danbi <silverwill39@gmail.com>
2022-12-25 21:02:58 -0300

    Lv.0 짝수의 합

--- 짝수의합.js
+++ 짝수의합.js
@@ -14,3 +14,14 @@ function solution(n) {
   answer = myArr.reduce((pre, cur) => (pre += cur), 0);
   return answer;
 }
+
+// function solution(n) {
+//   let answer = 0;
+
+//   for (let i = 0; i < n + 1; i++) {
+//     if (i % 2 === 0) {
+//       answer += i;
+//     }
+//   }
+//   return answer;
+// }
