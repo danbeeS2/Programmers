342084905b61e9c505212043730abeed71360277
Danbi <silverwill39@gmail.com>
2022-12-29 17:59:58 -0300

    Lv.0 양꼬치

---
+++ 양꼬치.js
@@ -0,0 +1,11 @@
+function solution(n, k) {
+  let price = n * 12000 + (k - Math.floor(n / 10)) * 2000;
+
+  return price;
+}
+console.log(solution(10, 3));
+console.log(solution(64, 6));
+
+101 * 12000;
+10 * 2000;
+10 * 2000;
