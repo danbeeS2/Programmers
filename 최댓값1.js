c8de725e3105292ae1154391d9a0a58a45d2d016
Danbi <silverwill39@gmail.com>
2023-01-02 01:03:40 -0300

    Lv.0 최댓값 만들기(1)

---
+++ 최댓값1.js
@@ -0,0 +1,6 @@
+function solution(numbers) {
+  numbers.sort((a, b) => b - a);
+  return numbers[0] * numbers[1];
+}
+console.log(solution([1, 2, 3, 4, 5])); // 20
+console.log(solution([0, 31, 24, 10, 1, 9])); // 744
