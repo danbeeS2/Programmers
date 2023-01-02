d156d791008ed765021cf939d7fcdc1e84744927
Danbi <silverwill39@gmail.com>
2023-01-01 23:59:23 -0300

    Lv.0 직각삼각형 출력하기

--- 직각삼각형.js
+++ 직각삼각형.js
@@ -16,3 +16,23 @@ rl.on("line", function (line) {
   }
   console.log(triangle.trim());
 });
+
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// let input = [];
+
+// rl.on('line', function (line) {
+//     input = line.split(' ');
+// }).on('close', function () {
+//     solution(Number(input[0]));
+// });
+
+// function solution(num){
+//   for (let i = 1; i <= num; i++) {
+//     console.log("*".repeat(i));
+//   }
+// }
