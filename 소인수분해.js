0ddc263d9a374e28c278da72905eb9b95fc2fd43
Danbi <silverwill39@gmail.com>
2023-01-05 20:22:58 -0300

    Lv.0 소인수분해

---
+++ 소인수분해.js
@@ -0,0 +1,26 @@
+// function solution(n) {
+//   let answer = [];
+//   let divisor = 2;
+
+//   while (n >= 2) {
+//     if (n % divisor === 0) {
+//       answer.push(divisor);
+//       n /= divisor;
+//     } else divisor++;
+//   }
+//   return [...new Set(answer)];
+// }
+console.log(solution(12)); // [2, 3]
+console.log(solution(17)); // [17]
+console.log(solution(420)); // [2, 3, 5, 7]
+
+function solution(n) {
+  let answer = [];
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      answer.push(i);
+      n /= i;
+    }
+  }
+  return answer;
+}
