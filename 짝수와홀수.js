5c3fa64785f59089502140fb709189998d0ecc66
Danbi <silverwill39@gmail.com>
2023-01-13 20:29:12 +0900

    Lv.1 짝수와 홀수

---
+++ 짝수와홀수.js
@@ -0,0 +1,10 @@
+function solution(num) {
+  return num % 2 === 0 ? "Even" : "Odd";
+}
+
+console.log(solution(3)); //	"Odd"
+console.log(solution(4)); //	"Even"
+
+// function evenOrOdd(num) {
+//   return num % 2 ? "Odd" : "Even";
+// }
