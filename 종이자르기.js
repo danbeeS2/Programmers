e01347aec4597544eae64e930f19d24d8579aebc
Danbi <silverwill39@gmail.com>
2023-01-22 15:17:10 +0900

    Lv.0 종이 자르기

---
+++ 종이자르기.js
@@ -0,0 +1,10 @@
+function solution(M, N) {
+  return M * N - 1;
+}
+
+console.log(solution(2, 2)); // 3
+console.log(solution(2, 5)); // 9
+console.log(solution(1, 1)); // 0
+// 2,2	3
+// 2,5	9
+// 1,1	0
